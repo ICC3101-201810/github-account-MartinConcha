e7c6de29d2f4e86d27d97e18f959b0b6701ba42c
MartinConcha <maconcha1@miuandes.cl>
2018-04-03 17:30:45 -0300

    First commit for april 3 lab

---
+++ ConsoleApp1/ConsoleApp1/Sucursal.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Sucursal
+    {
+        public string nombre;
+        public string direccion;
+        List <Vehiculos> vehiculos = new List<Vehiculos>();
+        List <Accesorios> accesorios = new List<Accesorios>();
+
+        public Sucursal(string Nombre, string Direccion)
+        {
+            nombre = Nombre;
+            direccion = Direccion;
+        }
+
+        public bool AgregarVehiculo(Vehiculos vehiculo)
+        {
+            vehiculos.Add(vehiculo);
+            return true;
+        }
+        public bool AgregarAccesorios(Accesorios accesorio)
+        {
+            accesorios.Add(accesorio);
+            return true;
+        }
+        public void VerAccesorios()
+        {
+            foreach (Accesorios accesorio in accesorios)
+            {
+                Console.WriteLine(accesorio.InfoAccesorio());
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Vehiculos.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Vehiculos
+    {
+        public string tipo;
+        public string modelo;
+        public string marca;
+        public string permiso;
+        public int precio;
+
+        public Vehiculos(string Tipo, string Nombre, string Marca, string Permiso, int Precio)
+        {
+            tipo = Tipo;
+            modelo = Nombre;
+            marca = Marca;
+            permiso = Permiso;
+            precio = Precio;
+        }
+        
+    }
+}
